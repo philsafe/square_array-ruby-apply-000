0de7f1f2257b15df528c7b2c9217a423a3d5612c
Philip Phipps <phippsphilip@yahoo.com>
2020-03-02 20:25:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
 #square_array_lab
 def
 numbers  = [1,2,3]
+
+square_array (numbers)
+#=>[1,4,9])
+
+new_numbers = [9,10,16,25]
+
+
  array.each {|i| numbers << i ** 2}
- numbers 
+ 
 end
-my_numbers = [1,2,3]
-p square_array_lab(array) #=> [1,4,9]
