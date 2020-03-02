9b4ae399d74410d866ff53a3b44a9e910d8c1938
Philip Phipps <phippsphilip@yahoo.com>
2020-03-02 19:29:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
+#square_array_lab
+def
+  array=
+  []
+  array.each { |i| array << i **2}
+  
+  array 
 end
+ numbers =
+
+  [1, 2, 3]
+
+def
+  
+  square_array(numbers)
+end  
